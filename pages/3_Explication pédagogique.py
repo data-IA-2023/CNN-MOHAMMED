3405ebc7baf559bd00d936e846881b0598be171f
COMEWLECOME <MOHASOFT@GMAIL.com>
2024-05-05 07:34:30 +0200

    avec explication

--- pages/3_Explication pédagogique.py
+++ pages/3_Explication pédagogique.py
@@ -1,1 +1,17 @@
 import streamlit as st
+
+def main():
+    st.title("Explication pédagogique")
+
+    # Chemin vers le fichier HTML téléchargé
+    chemin_fichier_html = "pages\CNN.html"  # Mettez à jour le chemin
+
+    # Lecture du fichier HTML
+    with open(chemin_fichier_html, 'r', encoding='utf-8') as f:
+        contenu_html = f.read()
+
+    # Affichage du contenu HTML
+    st.markdown(contenu_html, unsafe_allow_html=True)
+
+if __name__ == "__main__":
+    main()
